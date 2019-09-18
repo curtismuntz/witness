2ea2f40d9499986cdebe71eda59a74c187b6d514
Curtis Muntz <me@murt.is>
2019-09-18 12:24:18 -0700

    Unify all python client code, deprecate simple_gui

--- witness/client/endpoint_scripts/shell.py
+++ witness/client/endpoint_scripts/shell.py
@@ -3,21 +3,21 @@
 import os
 from cmd import Cmd
 
-import endpoint_scripts.common
-import endpoint_scripts.take_photo
-import endpoint_scripts.get_server_state
-import endpoint_scripts.start_recording
-import endpoint_scripts.stop_recording
-import endpoint_scripts.start_timelapse
-import endpoint_scripts.open_webcam
-import endpoint_scripts.clear_data
-import endpoint_scripts.start_monitor
-import endpoint_scripts.get_version
-import endpoint_scripts.list_files
-import endpoint_scripts.take_3
-import endpoint_scripts.automated_timelapse
-import endpoint_scripts.download_all
-import endpoint_scripts.set_rotation
+import common
+import take_photo
+import get_server_state
+import start_recording
+import stop_recording
+import start_timelapse
+import open_webcam
+import clear_data
+import start_monitor
+import get_version
+import list_files
+import take_3
+import automated_timelapse
+import download_all
+import set_rotation
 
 
 class MyPrompt(Cmd):
@@ -41,28 +41,28 @@ class MyPrompt(Cmd):
         Set the rotation of the camera. If the rotation is not 0 degrees, the server will rotate the
         images before they are consumed.
         '''
-        endpoint_scripts.set_rotation.set_rotation(self._service, *parse_int(input))
+        set_rotation.set_rotation(self._service, *parse_int(input))
 
     def do_open_webcam(self, input):
         '''open_webcam $webcam_id=1
 
         Sets the server to the specified webcam id (as listed in /dev/video{0,1,2,etc}).
         '''
-        endpoint_scripts.open_webcam.open_webcam(self._service, *parse_int(input))
+        open_webcam.open_webcam(self._service, *parse_int(input))
 
     def do_automated_timelapse(self, input):
         '''fnname args
 
         TODO(curtismuntz): add help
         '''
-        endpoint_scripts.automated_timelapse.automated_timelapse(self._service)
+        automated_timelapse.automated_timelapse(self._service)
 
     def do_clear_data(self, input):
         '''clear_data
 
         Clears the server's data directory.
         '''
-        endpoint_scripts.clear_data.clear_data(self._service)
+        clear_data.clear_data(self._service)
 
     def do_download_all(self, input):
         '''download_all
@@ -71,35 +71,35 @@ class MyPrompt(Cmd):
         directory.
         '''
         # TODO(curtismuntz): support parameter passing here
-        endpoint_scripts.download_all.download_all(self._service, self._file_server)
+        download_all.download_all(self._service, self._file_server)
 
     def do_get_server_state(self, input):
         '''get_server_state
 
         Prints the state of the server (Active, Idle)
         '''
-        endpoint_scripts.get_server_state.get_server_state(self._service)
+        get_server_state.get_server_state(self._service)
 
     def do_get_version(self, input):
         '''get_version
 
         Prints the version of witness running on the server
         '''
-        endpoint_scripts.get_version.get_version(self._service)
+        get_version.get_version(self._service)
 
     def do_list_files(self, input):
         '''list_files
 
         Lists the supported media files in the data directory of the server
         '''
-        endpoint_scripts.list_files.list_files(self._service)
+        list_files.list_files(self._service)
 
     def do_start_monitor(self, input):
         '''start_monitor
 
         Starts the monitoring service
         '''
-        endpoint_scripts.start_monitor.start_monitor(self._service)
+        start_monitor.start_monitor(self._service)
 
     def do_start_recording(self, input):
         '''start_recording
@@ -108,7 +108,7 @@ class MyPrompt(Cmd):
         generated.
         '''
         # TODO(curtismuntz): support parameter passing here
-        endpoint_scripts.start_recording.start_recording(self._service)
+        start_recording.start_recording(self._service)
 
     def do_start_timelapse(self, input):
         '''start_timelapse
@@ -117,14 +117,14 @@ class MyPrompt(Cmd):
         filename. If no filename is given, one will be generated.
         '''
         # TODO(curtismuntz): support parameter passing here
-        endpoint_scripts.start_timelapse.start_timelapse(self._service)
+        start_timelapse.start_timelapse(self._service)
 
     def do_stop_recording(self, input):
         '''stop_recording
 
         Stops any active service on the server (monitor, recording, timelapse, etc)
         '''
-        endpoint_scripts.stop_recording.stop_recording(self._service)
+        stop_recording.stop_recording(self._service)
 
     def do_take_3(self, input):
         '''take_3
@@ -132,7 +132,7 @@ class MyPrompt(Cmd):
         Takes 3 photos sequentially with a 1 second sleep in between photos.
         These photos will have generated file names.
         '''
-        endpoint_scripts.take_3.take_3(self._service)
+        take_3.take_3(self._service)
 
     def do_take_photo(self, input):
         '''take_photo
@@ -141,7 +141,7 @@ class MyPrompt(Cmd):
         will be generated.
         '''
         # TODO(curtismuntz): support parameter passing here
-        endpoint_scripts.take_photo.take_photo(self._service)
+        take_photo.take_photo(self._service)
 
 
 def parse_int(arg):
@@ -150,10 +150,10 @@ def parse_int(arg):
 
 
 def main():
-    arg_parser = endpoint_scripts.common.create_arg_parser()
-    grpc_server, file_server = endpoint_scripts.common.services(arg_parser)
+    arg_parser = common.create_arg_parser()
+    grpc_server, file_server = common.services(arg_parser)
 
-    service = endpoint_scripts.common.create_service(grpc_server)
+    service = common.create_service(grpc_server)
 
     p = MyPrompt(service, file_server)
     p.cmdloop()
